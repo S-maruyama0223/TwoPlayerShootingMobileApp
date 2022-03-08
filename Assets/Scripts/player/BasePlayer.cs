56a05a348ea19ab103bae309499f842e7f7205f8
S-maruyama0223 <wanshanxiangtailang0223@gmail.com>
2022-03-08 20:07:49 +0900

    キャラの動きまで完成

---
+++ Assets/Scripts/player/BasePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePlayer : MonoBehaviour
+{
+    public FloatingJoystick joystick;
+    public GroundCheck groundCheck;
+    private float moveSpeed = 10;
+    private Rigidbody2D rb;
+    // ジャンプ力
+    private float flap = 1500.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = joystick.Horizontal;
+        float y = joystick.Vertical;
+
+        if (x < 0)
+        {
+            // 左向き
+            transform.localScale = new Vector3(-1, 1, 1);
+        } else if (x > 0)
+        {
+            // 右向き
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (y > 0.5 && groundCheck.IsGround())
+        {
+            Jump();
+        } 
+
+        rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector2.up * this.flap);
+    }
+}
---
+++ Assets/Scripts/player/GroundCheck.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private string groundTag = "Ground";
+    private bool groundFlug = false;
+    private bool isGroundEnter, isGroundExit, isGroundStay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsGround()
+    {
+        
+        if (isGroundEnter || isGroundStay)
+        {
+            this.groundFlug = true;    
+        }
+        else if (isGroundExit)
+        {
+            this.groundFlug = false;
+        }
+        isGroundEnter = false;
+        isGroundStay = false;
+        isGroundExit = false;
+        return groundFlug;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundEnter = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundExit = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundStay = true;
+        }
+    }
+}
